55be54ec5ead796d2ac7e9916bb21d70c9a616b8
Hui Xiang <hui.xiang@canonical.com>
2014-09-18 20:56:48 +0800

    Remove haproxy checking

--- hooks/nova_compute_utils.py
+++ hooks/nova_compute_utils.py
@@ -475,12 +475,3 @@ def setup_ipv6():
     if ubuntu_rel < 14.04:
         raise Exception("IPv6 is not supported for Ubuntu "
                         "versions less than Trusty 14.04")
-
-    # NOTE(xianghui): Need to install haproxy(1.5.3) from trusty-backports
-    # to support ipv6 address, so check is required to make sure not
-    # breaking other versions, IPv6 only support for >= Trusty
-    if ubuntu_rel == 14.04:
-        add_source('deb http://archive.ubuntu.com/ubuntu trusty-backports'
-                   ' main')
-        apt_update()
-        apt_install('haproxy/trusty-backports', fatal=True)
