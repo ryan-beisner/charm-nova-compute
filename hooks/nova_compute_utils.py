0b84dc7da8e6eeb2ac621fa0da0aae019041a210
james.page@ubuntu.com <>
2015-07-14 12:11:25 +0100

    Alway set network_manager

--- hooks/nova_compute_utils.py
+++ hooks/nova_compute_utils.py
@@ -423,17 +423,15 @@ def network_manager():
     Obtain the network manager advertised by nova-c-c, renaming to Quantum
     if required
     '''
-    manager = None
-    if neutron_plugin_legacy_mode():
-        manager = _network_config().get('network_manager')
-        if manager:
-            manager = manager.lower()
-            if manager not in ['quantum', 'neutron']:
-                return manager
-            if os_release('nova-common') in ['folsom', 'grizzly']:
-                return 'quantum'
-            else:
-                return 'neutron'
+    manager = _network_config().get('network_manager')
+    if manager:
+        manager = manager.lower()
+        if manager not in ['quantum', 'neutron']:
+            return manager
+        if os_release('nova-common') in ['folsom', 'grizzly']:
+            return 'quantum'
+        else:
+            return 'neutron'
     return manager
 
 
