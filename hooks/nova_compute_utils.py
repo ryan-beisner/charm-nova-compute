49addcaaa8b31d8ebb5df6451b0cf798b2c993de
James Page <james.page@ubuntu.com>
2015-09-09 12:59:09 +0100

    Nope - do it as root

--- hooks/nova_compute_utils.py
+++ hooks/nova_compute_utils.py
@@ -622,10 +622,10 @@ def configure_lxd_host(settings, user):
     if ubuntu_release > "vivid":
         log('>= Wily deployment - configuring LXD trust password and address',
             level=INFO)
-        cmd = ['sudo', '-u', user, 'lxc', 'config', 'set',
+        cmd = ['lxc', 'config', 'set',
                'core.trust_password', settings['lxd_password']]
         check_call(cmd)
-        cmd = ['sudo', '-u', user, 'lxc', 'config', 'set',
+        cmd = ['lxc', 'config', 'set',
                'core.https_address', settings['lxd_address']]
         check_call(cmd)
     elif ubuntu_release == "vivid":
