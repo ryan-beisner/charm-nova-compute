ryan-beisner/charm-nova-compute

commit b533a3fbfb96bd5939b3e790f67964ce3ba8f7fd

Delete libvirt-bin default network to avoid MASQUERADE rules
libvirt-bin installs a 192.168.122.0/24 default network and creates
MASQUERADE rules for it on boot.  These rules will effect and break
instance traffic including GRE tenant networks.

Check if the network exists and then destroy it with virsh net-destroy
which both immediately removes the MASQUERADE rules and the network so
it is not applied after reboot.

Change-Id: Ia79aea6ef889d1ef58f903f967bea37dc07fd160
Closes-Bug: #1387390

hooks/nova_compute_hooks.py
@@ -17,6 +17,7 @@
 import platform
 import sys
 import uuid
+import os
 
 from charmhelpers.core.hookenv import (
     Hooks,
@@ -85,6 +86,7 @@ from nova_compute_utils import (
     get_hugepage_number,
     assess_status,
     set_ppc64_cpu_smt_state,
+    destroy_libvirt_network,
 )
 
 from charmhelpers.contrib.network.ip import (
@@ -146,6 +148,9 @@ def config_changed():
     sysctl_dict = config('sysctl')
     if sysctl_dict:
         create_sysctl(sysctl_dict, '/etc/sysctl.d/50-nova-compute.conf')
+
+    if os.path.exists('/etc/libvirt/qemu/networks/default.xml'):
+        destroy_libvirt_network('default')
 
     if migration_enabled() and config('migration-auth-type') == 'ssh':
         # Check-in with nova-c-c and register new ssh key, if it has just been

hooks/nova_compute_utils.py
@@ -571,6 +571,11 @@ def create_libvirt_secret(secret_file, secret_uuid, key):
     check_call(cmd)
 
 
+def destroy_libvirt_network(netname):
+    """Delete a network using virsh net-destroy"""
+    return subprocess.check_call(['virsh', 'net-destroy', netname])
+
+
 def configure_lxd(user='nova'):
     ''' Configure lxd use for nova user '''
     if not git_install_requested():

unit_tests/test_nova_compute_hooks.py
@@ -82,6 +82,7 @@ TO_PATCH = [
     'assert_libvirt_rbd_imagebackend_allowed',
     'is_request_complete',
     'send_request_if_needed',
+    'destroy_libvirt_network',
     # socket
     'gethostname',
     'create_sysctl',

commit 49390755ad8249b0994a8f41f81b1d7adb8960db

Fix issues introduced by previous commit
Commit b533a3fbfb96bd5939b3e790f67964ce3ba8f7fd introduces a regression
whereby subsequent runs of the config-changed hook will throw an error
when it tries to re-delete the default network.

Change-Id: I17c8ca7cda27ae2e282c0b2c1cd921c1a2ef3162

hooks/nova_compute_hooks.py
@@ -17,7 +17,6 @@
 import platform
 import sys
 import uuid
-import os
 
 from charmhelpers.core.hookenv import (
     Hooks,
@@ -149,8 +148,7 @@ def config_changed():
     if sysctl_dict:
         create_sysctl(sysctl_dict, '/etc/sysctl.d/50-nova-compute.conf')
 
-    if os.path.exists('/etc/libvirt/qemu/networks/default.xml'):
-        destroy_libvirt_network('default')
+    destroy_libvirt_network('default')
 
     if migration_enabled() and config('migration-auth-type') == 'ssh':
         # Check-in with nova-c-c and register new ssh key, if it has just been

hooks/nova_compute_utils.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+import re
 import shutil
 import pwd
 import subprocess
@@ -51,6 +52,7 @@ from charmhelpers.core.hookenv import (
     relation_get,
     DEBUG,
     INFO,
+    WARNING,
 )
 
 from charmhelpers.core.templating import render
@@ -573,7 +575,20 @@ def create_libvirt_secret(secret_file, secret_uuid, key):
 
 def destroy_libvirt_network(netname):
     """Delete a network using virsh net-destroy"""
-    return subprocess.check_call(['virsh', 'net-destroy', netname])
+    try:
+        out = check_output(['virsh', 'net-list']).split('\n')
+        if len(out) < 3:
+            return
+
+        for line in out[2:]:
+            res = re.search("^\s+{} ".format(netname), line)
+            if res:
+                check_call(['virsh', 'net-destroy', netname])
+                return
+
+    except CalledProcessError:
+        log("Failed to destroy libvirt network '{}'".format(netname),
+            level=WARNING)
 
 
 def configure_lxd(user='nova'):

unit_tests/test_nova_compute_utils.py
@@ -27,6 +27,12 @@ from test_utils import (
     patch_open
 )
 
+VIRSH_NET_LIST = """ Name                 State      Autostart     Persistent
+----------------------------------------------------------
+ somenet              active     yes           yes
+ default              active     yes           yes
+ altnet               active     yes           yes
+"""
 
 TO_PATCH = [
     'apt_install',
@@ -706,3 +712,19 @@ class NovaComputeUtilsTests(CharmTestCase):
             asf.assert_called_once_with('some-config')
             # ports=None whilst port checks are disabled.
             f.assert_called_once_with('assessor', services='s1', ports=None)
+
+    @patch.object(utils, 'check_call')
+    @patch.object(utils, 'check_output')
+    def test_destroy_libvirt_network(self, mock_check_output, mock_check_call):
+        mock_check_output.return_value = VIRSH_NET_LIST
+        utils.destroy_libvirt_network('default')
+        cmd = ['virsh', 'net-destroy', 'default']
+        mock_check_call.assert_has_calls([call(cmd)])
+
+    @patch.object(utils, 'check_call')
+    @patch.object(utils, 'check_output')
+    def test_destroy_libvirt_network_no_exist(self, mock_check_output,
+                                              mock_check_call):
+        mock_check_output.return_value = VIRSH_NET_LIST
+        utils.destroy_libvirt_network('defaultX')
+        self.assertFalse(mock_check_call.called)